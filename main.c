b2df83e7e9a5b98c0558a7865ab77c823795b075
mhaidar10 <72262185+mhaidar10@users.noreply.github.com>
2021-01-06 09:32:44 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
+#define trigPin 7
+#define echoPin 6
 #include <Servo.h>
-Servo myservo;
-int angle =0;
-int angleStep =50;
+Servo servokanan;
+Servo servokiri;
 
-void setup() { 
-  myservo.attach(8);
-  pinMode(2,INPUT_PULLUP);
+void setup()
+{
+    Serial.begin(9600);
+    servokanan.attach(5);
+    servokiri.attach(3);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
-void loop() {
-  if (digitalRead(2) == HIGH){
-myservo.write(180);
-  }
-  else {
-  myservo.write(-180);
-  }
+void loop()
+{
+    long duration, distance;
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration / 2) / 29.1;
+
+    if (distance <= 5) // jarak sensor
+    {
+        servokanan.write(100), servokiri.write(20);
+        delay(1000);
+    }
+
+    else
+    {
+        servokanan.write(10), servokiri.write(120); //sudut
+        delay(100);
+    }
 }
